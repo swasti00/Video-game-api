16ff0c38a5388152b659bb4628da796e206ea4b4
root01 <you@example.com>
2023-06-03 15:15:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [gameTitle, setGameTitle] = useState('')
+  const [seachedGame , setSeachedGame] = useState([])
+
+  const searchGame = () => {
+    fetch(
+      `https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`
+      ).then((response) => response.json())
+      .then((data) => {
+        setSeachedGame(data)
+        console.log(data)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='searchSection'>
+        <h1>Search For A Game</h1>
+        <input required type="text" placeholder="Search..." onChange={(e) => {
+          setGameTitle(e.target.value);
+        }}/>
+        <button onClick={searchGame}>Search Game</button>
+
+        <div className='games'>
+          {seachedGame.map((game , key) => {
+            return (
+              <div className='game' key={key}>
+                {game.external}
+                <img src={game.thumb} alt={game.internalName} />
+                {game.cheapest}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className='dealsSection'>
+        <h1>Latest Deals</h1>
+      </div>
     </div>
   );
 }
